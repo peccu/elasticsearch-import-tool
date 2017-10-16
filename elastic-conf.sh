54b9fded8875f32a58c94076adfd92ec8ba466c9
peccu <peccul@gmail.com>
2017-10-16 20:59:33 +0900

    extract elastic search configuration

---
+++ elastic-conf.sh
@@ -0,0 +1,21 @@
+ELASTIC_CURL_OPT=(--user elastic:changeme)
+ELASTIC_CURL_URL='http://localhost:9200'
+
+# Example
+# # INDEX=index-name
+# # curl ${ELASTIC_CURL_OPT[@]} -XDELETE $ELASTIC_CURL_URL/$INDEX
+# # curl ${ELASTIC_CURL_OPT[@]} -XPUT $ELASTIC_CURL_URL/$INDEX -d '{
+# #   "mappings": {
+# #     "mappingtype-ABC": {
+# #       "properties": {
+# #         "key1": {"type": "integer"},
+# #         "date": { "type": "date", "format": "yyyyMMdd HH:mm Z" }
+# #       }
+# #     }
+# #   }
+# # }'
+
+# bulk format example
+# # echo '{"index":{"_index":"index-name","_type":"mappingtype-ABC","_id":"unique-id-123"}}
+# # {"key1":"value1","id":"unique-id-123","date":"20171016 00:00 +0900"}
+# # ' | curl -s ${ELASTIC_CURL_OPT[@]} -XPOST $ELASTIC_CURL_URL/_bulk --data-binary @- >/dev/null
