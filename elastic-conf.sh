1563b41045470650081b7bfe666afe7ac3d50d36
peccu <peccul@gmail.com>
2017-10-17 13:40:07 +0900

    extract mapping type name

--- elastic-conf.sh
+++ elastic-conf.sh
@@ -3,10 +3,11 @@ ELASTIC_CURL_URL='http://localhost:9200'
 
 # Example
 # # INDEX=index-name
+# # MAP=mappingtype-ABC
 # # curl ${ELASTIC_CURL_OPT[@]} -XDELETE $ELASTIC_CURL_URL/$INDEX
 # # curl ${ELASTIC_CURL_OPT[@]} -XPUT $ELASTIC_CURL_URL/$INDEX -d '{
 # #   "mappings": {
-# #     "mappingtype-ABC": {
+# #     "'$MAP'": {
 # #       "properties": {
 # #         "key1": {"type": "integer"},
 # #         "date": { "type": "date", "format": "yyyyMMdd HH:mm Z" }
@@ -16,6 +17,6 @@ ELASTIC_CURL_URL='http://localhost:9200'
 # # }'
 
 # bulk format example
-# # echo '{"index":{"_index":"index-name","_type":"mappingtype-ABC","_id":"unique-id-123"}}
+# # echo '{"index":{"_index":"index-name","_type":"'$MAP'","_id":"unique-id-123"}}
 # # {"key1":"value1","id":"unique-id-123","date":"20171016 00:00 +0900"}
 # # ' | curl -s ${ELASTIC_CURL_OPT[@]} -XPOST $ELASTIC_CURL_URL/_bulk --data-binary @- >/dev/null
