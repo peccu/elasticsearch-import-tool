72cb19f28a0574a0ca2d11e55cfc491897bbfba4
peccu <peccul@gmail.com>
2017-10-17 13:41:56 +0900

    update mapping sample

--- elastic-conf.sh
+++ elastic-conf.sh
@@ -10,7 +10,8 @@ ELASTIC_CURL_URL='http://localhost:9200'
 # #     "'$MAP'": {
 # #       "properties": {
 # #         "key1": {"type": "integer"},
-# #         "date": { "type": "date", "format": "yyyyMMdd HH:mm Z" }
+# #         "unixtime": { "type": "date", "format": "epoch_second" },
+# #         "date": { "type": "date", "format": "yyyyMMdd HH:mm:ss Z" }
 # #       }
 # #     }
 # #   }
@@ -18,5 +19,5 @@ ELASTIC_CURL_URL='http://localhost:9200'
 
 # bulk format example
 # # echo '{"index":{"_index":"index-name","_type":"'$MAP'","_id":"unique-id-123"}}
-# # {"key1":"value1","id":"unique-id-123","date":"20171016 00:00 +0900"}
+# # {"key1":"value1","id":"unique-id-123","date":"20171016 00:00 +0900","unixtime": "1508168400"}
 # # ' | curl -s ${ELASTIC_CURL_OPT[@]} -XPOST $ELASTIC_CURL_URL/_bulk --data-binary @- >/dev/null
